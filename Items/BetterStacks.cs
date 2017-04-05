3d3a2dab40a54246bdc4acec86a1513bd541931b
Craig Crawford <craigcrawford1988@gmail.com>
2017-04-06 00:31:01 +0100

    * Added Glowsticks, Snowballs * Fixed a bug which prevented bombs and a few other things getting the stack bonuses.

--- Items/BetterStacks.cs
+++ Items/BetterStacks.cs
@@ -7,9 +7,28 @@ namespace BetterStacks.Items
 	{
 		public override void SetDefaults(Item item)
 		{
-			if (item.maxStack == 999)
+			// All grenades, bombs
+			// Excludes:
+			// * 453 - Bomb Statue
+			// * 758 - Grenade Launcher
+			// * 3837 - Etherian Goblin Bomber Banner
+			if (ContainsAny(item.name, "Grenade", "Bomb") && (item.type != 453 && item.type != 758 && item.type != 3837))
 			{
-				if (ContainsAny(item.name, "Arrow", "Block", "Bone", "Bottle", "Bullet", "Cobweb", "Fence", "Gel", "Paint", "Platform", "Rope", "Shuriken", "Wall", "Wood"))
+				item.maxStack = 999;
+			}
+
+			// All potions, dynamite
+			// Excludes:
+			// * 456 - Potion Statue
+			// * 2456 - Dynamite Fish
+			else if (ContainsAny(item.name, "Potion", "Dynamite") && (item.type != 456 && item.type != 2456))
+			{
+				item.maxStack = 99;
+			}
+
+			else if (item.maxStack == 999)
+			{
+				if (ContainsAny(item.name, "Arrow", "Block", "Bone", "Bottle", "Bullet", "Cobweb", "Fence", "Gel", "Paint", "Platform", "Rope", "Shuriken", "Snowball", "Wall", "Wood"))
 				{
 					item.maxStack = 9999;
 				}
@@ -25,7 +44,7 @@ namespace BetterStacks.Items
 
 			else if (item.maxStack == 99)
 			{
-				if (ContainsAny(item.name, "Acorn", "Bar", "Hook", "Torch"))
+				if (ContainsAny(item.name, "Acorn", "Bar", "Glowstick", "Hook", "Torch"))
 				{
 					item.maxStack = 999;
 				}
@@ -37,25 +56,6 @@ namespace BetterStacks.Items
 				{
 					item.maxStack = 999;
 				}
-			}
-
-			// All grenades, bombs
-			// Excludes:
-			// * 453 - Bomb Statue
-			// * 758 - Grenade Launcher
-			// * 3837 - Etherian Goblin Bomber Banner
-			else if (ContainsAny(item.name, "Grenade", "Bomb") && (item.type != 453 && item.type != 758 && item.type != 3837))
-			{
-				item.maxStack = 999;
-			}
-
-			// All potions, dynamite
-			// Excludes:
-			// * 456 - Potion Statue
-			// * 2456 - Dynamite Fish
-			else if (ContainsAny(item.name, "Potion", "Dynamite") && (item.type != 456 && item.type != 2456))
-			{
-				item.maxStack = 99;
 			}
 		}
 
