0c3579c063d26456cca120318fad73f9df7f3178
Craig Crawford <craigcrawford1988@gmail.com>
2017-04-06 22:46:51 +0100

    Completely rewrote plugin.

--- Items/BetterStacks.cs
+++ Items/BetterStacks.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -7,54 +8,79 @@ namespace BetterStacks.Items
 	{
 		public override void SetDefaults(Item item)
 		{
-			// All grenades, bombs
-			// Excludes:
-			// * 453 - Bomb Statue
-			// * 758 - Grenade Launcher
-			// * 3837 - Etherian Goblin Bomber Banner
-			if (ContainsAny(item.name, "Grenade", "Bomb") && (item.type != 453 && item.type != 758 && item.type != 3837))
-			{
-				item.maxStack = 999;
-			}
+			// ExcludedItemIDs
+			// Only applies to ItemNamesToX not ItemIDsToX
 
-			// All potions, dynamite
-			// Excludes:
-			// * 456 - Potion Statue
-			// * 2456 - Dynamite Fish
-			else if (ContainsAny(item.name, "Potion", "Dynamite") && (item.type != 456 && item.type != 2456))
-			{
-				item.maxStack = 99;
-			}
+			// ItemID		Item Name							Which list?				Why?
+			// 342			Tiki Torch							ItemNamesTo999			Torch
+			// 343			Barrel								ItemNamesTo999			Bar
+			// 682			Marrow								ItemNamesTo9999			Arrow
+			// 758			Grenade Launcher					ItemNamesTo999			Grenade
+			// 1706			Bar Stool							ItemNamesTo999			Bar
+			// 1715			Bar									ItemNamesTo999			Bar
+			// 2456			Dynamite Fish						ItemNamesTo99			Dynamite
+			// 2485			Infected Scabbardfish				ItemNamesTo999			Bar
+			// 2488			Tropical Barracuda					ItemNamesTo999			Bar
+			// 3288			Lazure's Barrier Platform			ItemNamesTo999			Bar
+			int[] ExcludedItemIDs = { 342, 343, 682, 758, 1706, 1715, 2456, 2485, 2488, 3288 };
 
-			else if (item.maxStack == 999)
+			string[] ExcludedItemNames = { "Banner", "Statue" };
+			string[] ItemNamesTo99 = { "Dynamite", "Potion" };
+			string[] ItemNamesTo999 = { "Bar", "Bomb", "Glowstick", "Grenade", "Torch" };
+			string[] ItemNamesTo9999 = { "Arrow", "Block", "Bullet", "Fence", "Javelin", "Platform", "Rope", "Wall" };
+
+			// ItemIDsTo99:
+			// 1134			Bottled Honey
+			int[] ItemIDsTo99 = { 1134 };
+
+			// ItemIDsTo999:
+			// 27			Acorn
+			// 118			Hook
+			// 320			Feather
+			// 1328			Turtle Scale
+			// 1329			Tissue Sample
+			// 1330			Vertebrae
+			// 1516			Giant Harpy Feather
+			// 1517			Bone Feather
+			// 1518			Fire Feather
+			// 1519			Ice Feather
+			int[] ItemIDsTo999 = { 27, 118, 320, 1328, 1329, 1330, 1516, 1517, 1518, 1519 };
+
+			// ItemIDsTo9999:
+			// 9			Wood
+			// 23			Gel
+			// 31			Bottle
+			// 42			Shuriken
+			// 97			Musket Ball
+			// 126			Bottled Water
+			// 150			Cobweb
+			// 154			Bone
+			// 279			Throwing Knife
+			// 619			Ebonwood
+			// 621			Pearlwood
+			// 911			Shadewood
+			// 949			Snowball
+			// 1729			Spooky Wood
+			// 2260			Dynasty Wood
+			// 2503			Boreal Wood
+			// 2504			Palm Wood
+			// 3111			Pink Gel
+			// 3379			Bone Throwing Knife
+			int[] ItemIDsTo9999 = { 9, 23, 31, 42, 97, 126, 150, 154, 279, 619, 621, 911, 949, 1729, 2260, 2503, 2504, 3379 };
+
+			if (!ExcludedItemIDs.Contains(item.type) && !ContainsAny(item.name, ExcludedItemNames))
 			{
-				if (ContainsAny(item.name, "Arrow", "Block", "Bone", "Bottle", "Bullet", "Cobweb", "Fence", "Gel", "Paint", "Platform", "Rope", "Shuriken", "Snowball", "Wall", "Wood"))
+				if (ContainsAny(item.name, ItemNamesTo99) || ItemIDsTo99.Contains(item.type))
 				{
-					item.maxStack = 9999;
+					item.maxStack = 99;
 				}
-
-				// * 97 - Musket Ball
-				// * 279 - Throwing Knife
-				// * 3379 - Bone Knife
-				else if (item.type == 97 || item.type == 279 || item.type == 3379)
-				{
-					item.maxStack = 9999;
-				}
-			}
-
-			else if (item.maxStack == 99)
-			{
-				if (ContainsAny(item.name, "Acorn", "Bar", "Glowstick", "Hook", "Torch"))
+				else if (ContainsAny(item.name, ItemNamesTo999) || ItemIDsTo999.Contains(item.type))
 				{
 					item.maxStack = 999;
 				}
-
-				// 1328 - Turtle Scale
-				// 1329 - Tissue Sample
-				// 1330 - Vertebrae
-				else if (item.type >= 1328 && item.type <= 1330)
+				else if (ContainsAny(item.name, ItemNamesTo9999) || ItemIDsTo9999.Contains(item.type))
 				{
-					item.maxStack = 999;
+					item.maxStack = 9999;
 				}
 			}
 		}
