97bbdd5eeef132e618a71342f341f3f98f5ffc44
Craig Crawford <craigcrawford1988@gmail.com>
2017-04-17 18:07:09 +0100

    2.1.0 - Add more items

--- Items/BetterStacks.cs
+++ Items/BetterStacks.cs
@@ -33,6 +33,7 @@ namespace BetterStacks.Items
 
 			// ItemIDsTo999:
 			// 27			Acorn
+			// 86			Shadow Scale
 			// 118			Hook
 			// 320			Feather
 			// 1328			Turtle Scale
@@ -42,18 +43,22 @@ namespace BetterStacks.Items
 			// 1517			Bone Feather
 			// 1518			Fire Feather
 			// 1519			Ice Feather
-			int[] ItemIDsTo999 = { 27, 118, 320, 1328, 1329, 1330, 1516, 1517, 1518, 1519 };
+			int[] ItemIDsTo999 = { 27, 86, 118, 320, 1328, 1329, 1330, 1516, 1517, 1518, 1519 };
 
 			// ItemIDsTo9999:
+			// 5			Mushroom
 			// 9			Wood
 			// 23			Gel
 			// 31			Bottle
 			// 42			Shuriken
+			// 60			Vile Mushroom
 			// 97			Musket Ball
 			// 126			Bottled Water
 			// 150			Cobweb
 			// 154			Bone
+			// 183			Glowing Mushroom
 			// 279			Throwing Knife
+			// 530			Wire
 			// 619			Ebonwood
 			// 621			Pearlwood
 			// 911			Shadewood
@@ -63,9 +68,10 @@ namespace BetterStacks.Items
 			// 2260			Dynasty Wood
 			// 2503			Boreal Wood
 			// 2504			Palm Wood
+			// 2887			Vicious Mushroom
 			// 3111			Pink Gel
 			// 3379			Bone Throwing Knife
-			int[] ItemIDsTo9999 = { 9, 23, 31, 42, 97, 126, 150, 154, 279, 619, 621, 911, 931, 949, 1729, 2260, 2503, 2504, 3379 };
+			int[] ItemIDsTo9999 = { 5, 9, 23, 31, 42, 60, 97, 126, 150, 154, 183, 279, 530, 619, 621, 911, 931, 949, 1729, 2260, 2503, 2504, 2887, 3379 };
 
 			if (!ExcludedItemIDs.Contains(item.type) && !ContainsAny(item.name, ExcludedItemNames))
 			{
